86e9bf4829c5a66a8a1ead42ea3c70265e22b834
alrang0929 <159221214+alrang0929@users.noreply.github.com>
2024-11-17 23:41:48 +0900

    메모이제이션2

--- 2024_React기초+심화/03_memoization/use-memo-app/use-memo-app/src/components/HavyComponent.jsx
+++ 2024_React기초+심화/03_memoization/use-memo-app/use-memo-app/src/components/HavyComponent.jsx
@@ -1,17 +1,26 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 
 const HavyComponent = () => {
   const [value,setValue] = useState(0);
+  const havyWork = () => {
+    // 이렇게 무거운 놈은 실행시간이 길어 다음 실행까지 오래걸림.. 따라서 캐싱처리 필요 어떻게?
+
+    for(let i = 0 ; i<10000;i++ ){} //for
+    return 100;
+  }; //havyWork
+  const sampleValue = useMemo(()=>havyWork(),[]); // 이렇게 캐싱, 이것도 의존성 심을 수 있음
   // 코드 리턴구역/////////////////////////
   return (
     <>
-    <div>HavyComponent</div>
-    <p>나는{value}를 가져오는 엄청 무거운 컴포넌트야</p>
+    <div>나는 {sampleValue}를 가져오는 엄청 무거운 녀석</div>
     <button
-    onClick={setValue(value+1)}
-    >이 버튼을 누르면 count가 올라가요</button>
+    onClick={()=>{setValue(value +1)}}
+    >
+      나를 누르면 count가 올라감
+    </button>
     <br />
     {value}
+    
     </>
   )
 }
