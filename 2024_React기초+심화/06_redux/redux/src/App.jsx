dbc651ebb2368b4e9e91ca6132bcf2e1dc5abcf4
sellbell07@gmail.com <sellbell07@gmail.com>
2024-11-22 19:11:18 +0900

    redux 설치

--- 2024_React기초+심화/06_redux/redux/src/App.jsx
+++ 2024_React기초+심화/06_redux/redux/src/App.jsx
@@ -1,14 +1,17 @@
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 
 function App() {
   // redux를 써봅쉬다
-  const counterReducer = useSelector((state)=>{
-    return state.counter //초기값 설정한 객체 들어감
-    });
-  console.log("state",counterReducer);
+  // const counterReducer = useSelector((state)=>{
+  //   return state.counter //초기값 설정한 객체 들어감
+  //   });
+  // console.log("state",counterReducer);
+
+const dispatch = useDispatch();
+
   return (
   <>
-  <h1>App</h1>
+  <button>+1</button>
   </>
 );
 }
