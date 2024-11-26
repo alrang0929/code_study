9434391d8c5ee48124287197598f0dc31cd1f31f
sellbell07@gmail.com <sellbell07@gmail.com>
2024-11-26 18:32:30 +0900

    reducer

--- 2024_React기초+심화/06_redux/redux/src/App.jsx
+++ 2024_React기초+심화/06_redux/redux/src/App.jsx
@@ -1,17 +1,36 @@
-// import { useSelector } from "react-redux";
-
+import { useSelector } from "react-redux";
+import { useDispatch } from 'react-redux';
+import { addNumber, removeNumber } from "./redux/modules/counter";
+import { useState } from "react";
+// import { minusOne, plusOne } from "./redux/modules/counter";
 function App() {
+  const [count, setCount] = useState(0);
   // redux를 써봅쉬다
-  // const counterReducer = useSelector((state)=>{
-  //   return state.counter //초기값 설정한 객체 들어감
-  //   });
-  // console.log("state",counterReducer);
+  const counterReducer = useSelector((state)=>state.counter //초기값 설정한 객체 들어감
+    );
+  console.log("state",counterReducer);
 
-const dispatch = useDispatch();
+  // dispatch란? 액션객체를 리듀서로 보내는 전달자 역할
+  // 액션객체 type는 상수(전부 대문자)로 작성
+  // 리듀서란? 디스패치를 통해 전달받은 액션객체를 검사하고, 조건이 일치하면 새로운 상태값을 만들어 내는 "변화를 만들어내는"함수
+
+const dispatch = useDispatch(); //액션객체를 reducer에게 보내는 역할
 
   return (
   <>
-  <button>+1</button>
+  {counterReducer.number} 
+  <br />
+  <input type="number" value={count} />
+  <button onClick={()=>{
+    dispatch(addNumber(1));
+    //onClick 이벤트 발생> reducer로 액션 객체가 날라감
+  
+  }}>더하기</button>
+  <button onClick={()=>{
+    dispatch(removeNumber(1));
+    //onClick 이벤트 발생> reducer로 액션 객체가 날라감
+  
+  }}>빼기</button>
   </>
 );
 }
--- 2024_React기초+심화/06_redux/redux/src/redux/modules/counter.js
+++ 2024_React기초+심화/06_redux/redux/src/redux/modules/counter.js
@@ -3,16 +3,40 @@
 // 1. store에 들어갈 초기값을 생성한다. (useState 생성할 초기값 설정하는거랑 같은 이치)
 const initialState = {
     //초기값이 꼭 객체일 필요 X
-    Number: 0,
+    number: 0,
 }
+// const PLUS_ONE = "PLUS_ONE";
+// const MINUS_ONE = "MINUS_ONE";
+const ADD_NUMBER = "ADD_NUMBER";
+const REMOVE_NUMBER = "REMOVE_NUMBER";
+export const removeNumber = (payload) =>{
+    return{
+        type: REMOVE_NUMBER,
+        payload,
+    }
+}
+// payload = 변경할 값
+export const addNumber = (payload) =>{
+    return{
+        type: ADD_NUMBER,
+        payload,
+    }
+}
+
 // 2. reducer 함수 인자 2개 받음. 사용 방법임 ㄱㄱ
 // const counter = (state, action <= type이라는 값을 가지고 있는 객체 ) => {}; 
 // reducer 는 무슨 함수? 변화를 일으키는 함수!
 // 변화의 종류는 action.type에 담겨있음
 // sotre에서 (1)어떤 변화를 일으킬건지 (2)얼만큼의 변화를 일으킬건지가 명시되어있음
+
 const counter = (state=initialState, action) => {
+    // console.log("action=>", action); //{type: 'PLUS_ONE'}
+    //따라서 type에 따라 역할을 하게  하면됨!
     switch (action.type) {
- 
+        case ADD_NUMBER:
+            return {number: state.number + action.payload};
+        case REMOVE_NUMBER:
+            return {number: state.number - action.payload};
         default:
             // 리듀서 함수는 항상 return문으로 끝내기
            return state;
