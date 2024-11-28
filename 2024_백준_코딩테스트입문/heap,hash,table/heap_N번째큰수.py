05f0bf3b656dcdfbb233ba6da2c6cbb99d2ade2a
sellbell07@gmail.com <sellbell07@gmail.com>
2024-11-28 14:41:59 +0900

    폴더 이동

---
+++ 2024_백준_코딩테스트입문/heap,hash,table/heap_N번째큰수.py
@@ -0,0 +1,19 @@
+# https://www.acmicpc.net/problem/2075
+'''
+첫번째 줄에 N번째 큰 수를 출력한다
+
+5
+12 7 9 15 5
+13 8 11 19 6
+21 10 26 31 16
+48 14 28 35 25
+52 20 32 41 49
+
+[결과]
+35
+
+'''
+
+import sys
+import heapq 
+sys.stdin = open("input.txt","r")
