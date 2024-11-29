8fc6b0a494e6f96c8a3677b3d52fdafd4088ed38
sellbell07@gmail.com <sellbell07@gmail.com>
2024-11-29 21:01:32 +0900

    asd

--- 2024_백준_코딩테스트입문/3일차_heap,hash,table/asdasd.py
+++ 2024_백준_코딩테스트입문/3일차_heap,hash,table/asdasd.py
@@ -1,20 +1,15 @@
 import heapq
-# import sys
+import sys
 
-# 입력 처리
-# sys.stdin = open("input.txt", "r")  # 파일에서 입력 받기
-n = int(input())  # 후보의 수
-dasom = int(input())  # 다솜의 표
-votes = []  # 다른 후보들의 표
-
-# 나머지 후보들의 표 입력
-for _ in range(n - 1):
-    votes.append(int(input()))
-
-count = 0  # 매수 횟수 초기화
+# 1. 입력받기
+data = sys.stdin.read().splitlines()
+n = int(data[0])  # 후보의 수
+dasom = int(data[1])  # 다솜의 표
+votes = list(map(int, data[2:]))  # 다른 후보들의 표
+count = 0  # 매수 횟수
 
 # 1-2. votes를 최대 힙으로 관리하기 위한 음수로 저장
-max_heap = [-v for v in votes]  # 음수로 변환하여 최대 힙 구성
+max_heap = [-v for v in votes]  # 최대 힙을 위해 음수로 변환
 heapq.heapify(max_heap)  # 리스트를 힙 구조로 변환
 
 # 2. 예외 처리: 후보가 다솜 혼자인 경우
@@ -34,3 +29,4 @@ else:
             heapq.heappush(max_heap, -max_votes)
 
     print(count)
+
